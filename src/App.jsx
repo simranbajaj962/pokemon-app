1e4a17fd981aeb4ed4c89e7024b1edfd260c9ff3
Arif Imran <arifimran505@gmail.com>
2023-04-03 10:53:50 +0530

    changed input to lowercase and add back button on error page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Profile from "./components/Profile";
-import { useEffect, useState } from "react";
-import Stats from "./components/Stats";
-import Search from "./components/Search";
+import Profile from './components/Profile';
+import { useEffect, useState } from 'react';
+import Stats from './components/Stats';
+import Search from './components/Search';
 
 function App() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [id, setId] = useState(1);
   const [error, setError] = useState(false);
   const [pokemon, setPokemon] = useState();
@@ -15,7 +15,7 @@ function App() {
 
     const fetchPokemon = async () => {
       const res = await fetch(
-        `https://pokeapi.co/api/v2/pokemon/${name !== "" ? name : id}`
+        `https://pokeapi.co/api/v2/pokemon/${name !== '' ? name : id}`
       );
 
       if (!res.ok) {
@@ -44,12 +44,15 @@ function App() {
   if (!pokemon) return null;
 
   return (
-    <div className="mx-auto max-w-[44rem] px-6">
+    <div className='mx-auto max-w-[44rem] px-6'>
       <Search getInput={getInput} />
       {error && (
-        <h1 className="text-center mt-10 font-bold capatalize text-5xl text-teal-400">
-          No data found !
-        </h1>
+        <div>
+          <h1 className='text-center mt-10 font-bold capatalize text-5xl text-teal-400'>
+            No data found !
+          </h1>
+          <button onClick={() => setError(false)}>Go back</button>
+        </div>
       )}
       {!error && (
         <>
@@ -62,47 +65,47 @@ function App() {
           />
           <Stats stats={pokemon.stats} />
 
-          <div className="space-x-4 mt-6">
+          <div className='space-x-4 mt-6'>
             <button
-              className="bg-gray-200 rounded-full p-2"
+              className='bg-gray-200 rounded-full p-2'
               onClick={() => {
-                setName("");
+                setName('');
                 setId(id - 1);
               }}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
               >
-                <polyline points="15 18 9 12 15 6"></polyline>
+                <polyline points='15 18 9 12 15 6'></polyline>
               </svg>
             </button>
             <button
-              className="bg-gray-200 rounded-full p-2"
+              className='bg-gray-200 rounded-full p-2'
               onClick={() => {
-                setName("");
+                setName('');
                 setId(id + 1);
               }}
             >
               <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
               >
-                <polyline points="9 18 15 12 9 6"></polyline>
+                <polyline points='9 18 15 12 9 6'></polyline>
               </svg>
             </button>
           </div>
